ae8ab704899bd3d7409c559fbc4ad5ebea777a9c
Dao Danh Luu <luubkk57@gmail.com>
2016-08-02 16:24:28 +0700

    admin view all user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,16 +5,32 @@ class Admin::UsersController < ApplicationController
     @user = User.new
   end
 
+  def index
+    @trainees = User.trainee
+    @supervisors = User.supervisor
+  end
+
   def create
     @user = User.new user_params
     if @user.save
       flash[:success] = t "flash.user_created"
-      redirect_to root_path
+      redirect_to admin_users_path
     else
       get_role
       flash[:danger] = t "flash.user_notcreated"
       render :new
     end
+  end
+
+  def destroy
+    @user = User.find_by id: params[:id]
+    if @user.nil?
+      flash[:danger] = t "detele_user_fail"
+      redirect_to root_path
+    end
+    @user.delete
+    flash[:danger] = t "flash.delete_user"
+    redirect_to admin_users_path
   end
 
   private
